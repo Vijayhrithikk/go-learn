1d67e91b165f8ee8be152247a2602df1fe72945d
Vijayhrithikk <vijaymonish11@gmail.com>
2026-05-12 22:16:18 +0530

    fib defer understanding

--- defer.go
+++ defer.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
 func main() {
 	defer fmt.Println("This is Second")
 	defer fmt.Println("This is first to execute")
+	fmt.Println(fib(5))
 }
